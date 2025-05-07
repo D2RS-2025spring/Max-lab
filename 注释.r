3536249ae3ba56c75c9512e0a0a865e8b63666eb
zcx5378 <1604437912@qq.com>
2025-05-07 18:05:10 +0800

    Add files via upload

---
+++ 注释.r
@@ -0,0 +1,19 @@
+---
+title: "文献复现"
+author: "max"
+date: now
+output: html_document
+---
+
+# 分组数据分析实战
+
+在这一部分，将以论文中的数据分析为例，展示分组数据分析和可视化的重复性研究。首先，我们简单介绍一下论文的研究背景、方法和主要结果。然后，使用原始数据进行可重复研究，通过复现论文中的图片，展示分组数据分析和可视化的重复性研究。
+
+## 论文研究概述
+
+## 数据准备
+
+论文的原始数据及分析代码都在 [GitHub](https://github.com/daniosro/Si_biomineralization_ANME_SRB.git) 上。首先，使用 Git 命令将代码克隆到本地：
+
+```bash
+git clone https://github.com/daniosro/Si_biomineralization_ANME_SRB.git --depth 1
